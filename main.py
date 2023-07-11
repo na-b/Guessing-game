375696761904cdd9673bfb6594a8bf3ae01e45e8
na-b <nanabamen@gmail.com>
2023-07-11 10:53:44 +0000

    Gave the player 5 chances

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import random 
 
 guess_number=random.randint(1,20)
-user_input= int(input('Enter a number between 1 and 20: '))
-if user_input==guess_number:
+i=0
+while i<5:
+ user_input= int(input('Enter a number between 1 and 20: '))
+ if user_input==guess_number:
     print('Your guess is correct')
-else:
+ else:
     print("Your guess is wrong")
+ i+=1
