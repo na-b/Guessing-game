9dc122730076dd4bcb77656604c624f2051b363b
na-b <nanabamen@gmail.com>
2023-07-11 10:55:58 +0000

    Added a loop break

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ while i<5:
  user_input= int(input('Enter a number between 1 and 20: '))
  if user_input==guess_number:
     print('Your guess is correct')
+    break
  else:
     print("Your guess is wrong")
  i+=1
