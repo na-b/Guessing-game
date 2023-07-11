6a92e9d2aa1d6c1c8f20742df86a14e5bdfecb3b
na-b <nanabamen@gmail.com>
2023-07-11 10:17:36 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+guess_number=9
+user_input= int(input('Enter a number between 1 and 20: '))
+if user_input==guess_number:
+    print('Your guess is correct')
+else:
+    print("Your guess is wrong")
