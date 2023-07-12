0a87e19199d57e4231c842a28939d75cff97f0eb
na-b <nanabamen@gmail.com>
2023-07-12 10:07:21 +0000

    Added a loop break Prints the number of chances used Gave hints Added an ending message

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ i=0
 while i<5:
  user_input= int(input('Enter a number between 1 and 20: '))
  if user_input==guess_number:
-    print('Your guess is correct')
+    print("Your guess is correct!")
+    print("You won using",i+1,"out of 5 chances")
     break
  else:
-    print("Your guess is wrong")
+    if i==4:
+      print("Your guess is wrong and the game is over")
+    elif user_input<guess_number-5:
+      print("Your guess is too low, try again")
+    elif user_input>guess_number+5:
+      print("Your guess is too high, try again")
+    else:
+      print("Your guess is close")
+   
  i+=1
+print("Thanks for playing!")
